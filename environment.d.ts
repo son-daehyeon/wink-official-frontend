662312192aa40796dd1fb33da434c78d6a62061a
Son Daehyeon <sondaehyeon01@gmail.com>
2025-06-18 05:08:08 +0900

    refactor(kakao): KakaoSDK 삭제

--- environment.d.ts
+++ environment.d.ts
@@ -2,14 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       API_URL: string;
-      NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: string;
-      NEXT_PUBLIC_KAKAO_TEMPLATE_ID: string;
     }
-  }
-
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    Kakao: any;
   }
 }
 
